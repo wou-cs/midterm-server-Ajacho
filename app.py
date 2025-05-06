bca9972dba7f58b076a3d2d6bcbfb52b44aa6a70
ajacho <belen.jacho18@gmail.com>
2025-05-05 21:24:54 -0700

    Implement APIs for question 1

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+@app.route("/api/calcs/<int:number>", methods=["GET"])
+def get_calcs(number):
+    # First calc: decrement the number
+    dec = number - 1
+    hex_value = hex(number)
+
+    # Second calc: convert to hex
+    response = {
+        "dec": dec,
+        "hex": hex_value
+    }
+    return response, 200
+
+@app.errorhandler(404)
+def handle_bad_request(e):
+    return {"error": "Bad Request"}, 400
